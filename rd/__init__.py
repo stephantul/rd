aa463f887a7785f19c06c25803f7b8548a912c52
stephantul <stephan.tulkens@uantwerpen.be>
2018-12-21 10:00:47 +0100

    remove wordkit dependencies

--- rd/__init__.py
+++ rd/__init__.py
@@ -1,4 +1,4 @@
 """Representation Density."""
-from .rd import rd, rd_features
+from .rd import rd
 
-__all__ = ["rd", "rd_features"]
+__all__ = ["rd"]
--- rd/rd.py
+++ rd/rd.py
@@ -1,56 +1,19 @@
 """Representation Distance."""
 import numpy as np
 
+from tqdm import tqdm
 from sklearn.metrics import pairwise_distances
-from wordkit.features import fourteen, dislex, sixteen
-from wordkit.feature_extraction import OneHotCharacterExtractor
-from wordkit.transformers import WickelTransformer, \
-                                 ConstrainedOpenNGramTransformer, \
-                                 WeightedOpenBigramTransformer, \
-                                 OpenNGramTransformer, \
-                                 LinearTransformer
-
-
-def rd_features(words,
-                n=20,
-                feature_name="one hot",
-                metric="cosine",
-                memory_safe=False):
-    """Return the RD of some words fit with some features."""
-    X = select_features(words, feature_name)
-    return rd(X, n, memory_safe=memory_safe, metric=metric)
-
-
-def select_features(words, feature_name):
-    """Select a feature set and fit the words using that feature set."""
-    if feature_name == "one hot":
-        f = LinearTransformer(OneHotCharacterExtractor, field=None)
-    elif feature_name == "constrained open ngrams":
-        f = ConstrainedOpenNGramTransformer(field=None)
-    elif feature_name == "weighted open bigrams":
-        f = WeightedOpenBigramTransformer((.1, .8, .2), field=None)
-    elif feature_name == "open ngrams":
-        f = OpenNGramTransformer(field=None)
-    elif feature_name == "fourteen":
-        f = LinearTransformer(fourteen, field=None)
-    elif feature_name == "sixteen":
-        f = LinearTransformer(sixteen, field=None)
-    elif feature_name == "dislex":
-        f = LinearTransformer(dislex, field=None)
-    elif feature_name == "wickel":
-        f = WickelTransformer(n=3, field=None)
-    else:
-        raise ValueError("not recognized")
-
-    X = f.fit_transform(words)
-    return X
 
 
 def _cosine_safe(X, Y):
     """Memory safe cosine. Slow."""
-    dists = np.zeros((X.shape[0], X.shape[0]))
-    X = X / np.linalg.norm(X, axis=1)[:, None]
-    for x in range(0, len(X), 100):
+    dists = np.zeros((X.shape[0], Y.shape[0]))
+    X = X.astype(np.float32)
+    Y = Y.astype(np.float32)
+    if X is not Y:
+        Y /= np.linalg.norm(Y, axis=1)[:, None]
+    X /= np.linalg.norm(X, axis=1)[:, None]
+    for x in tqdm(range(0, len(X), 100)):
         dists[x:x+100] = 1 - X[x:x+100].dot(Y.T)
     return dists
 
@@ -58,7 +21,7 @@ def _cosine_safe(X, Y):
 def _euclidean_safe(X, Y):
     """Memory safe euclidean distance."""
     dists = []
-    for x in range(0, len(X), 100):
+    for x in tqdm(range(0, len(X), 100)):
         batch = X[x:x+100]
         diff = batch[:, None, :] - Y[None, :, :]
         dists.extend(np.linalg.norm(diff, axis=-1))
@@ -125,7 +88,7 @@ def rd(X, Y=None, n=20, memory_safe=False, metric="cosine"):
     d = np.partition(dists, kth=largest_n+1, axis=1)[:, :largest_n+1]
     d = np.sort(d, axis=1)
     for x in n:
-        out.append(d[:, 1:x+1].mean(1))
+        out.append(d[:, :x+1].sum(1))
 
     if was_int:
         out = out[0]
